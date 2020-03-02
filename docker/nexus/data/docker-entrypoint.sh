55d1e68ee7560ef35ddbfb61038a2a79317c4a93
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-03-02 13:50:15 +0700

    Add delay

--- docker/nexus/data/docker-entrypoint.sh
+++ docker/nexus/data/docker-entrypoint.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+PARAMS=""
+
+if [ ! -z "$NEXUS_RETRY" ]; then
+  PARAMS="$PARAMS --retry-connrefused --retry $NEXUS_RETRY"
+  if [ !-z "$NEXUS_RETRY_DELAY" ]; then
+    PARAMS="$PARAMS --retry-delay $NEXUS_RETRY_DELAY"
+  fi
+fi
+
 NEXUS_DEFAULT_PASSWORD=`cat /nexus-data/admin.password`
-curl -v -X POST -u admin:$NEXUS_DEFAULT_PASSWORD --header "Content-Type: text/plain" --data "$NEXUS_PASSWORD" "http://localhost:8081/service/rest/v1/script/admin/run"
+curl -v -X POST -u admin:$NEXUS_DEFAULT_PASSWORD $PARAMS --header "Content-Type: text/plain" --data "$NEXUS_PASSWORD" "http://localhost:8081/service/rest/v1/script/admin/run"

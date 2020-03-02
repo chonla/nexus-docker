51d92560404d9aca68e382c85dcc24fd4cea14ad
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-03-02 14:09:39 +0700

    Fix delay

--- docker/nexus/data/docker-entrypoint.sh
+++ docker/nexus/data/docker-entrypoint.sh
@@ -9,5 +9,9 @@ if [ ! -z "$NEXUS_RETRY" ]; then
   fi
 fi
 
+# wait until service is up
+curl --retry-connrefused --retry 20 --retry-delay 8 http://127.0.0.1
+
+# continue setting up
 NEXUS_DEFAULT_PASSWORD=`cat /nexus-data/admin.password`
 curl -v -X POST -u admin:$NEXUS_DEFAULT_PASSWORD $PARAMS --header "Content-Type: text/plain" --data "$NEXUS_PASSWORD" "http://localhost:8081/service/rest/v1/script/admin/run"
